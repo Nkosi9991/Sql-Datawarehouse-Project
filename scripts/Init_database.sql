a278382402bca8c4765f2e1205b7fef51e224d18
Nkosinathi Vusumuzi Mjilo <nkosinathivusu555@gmail.com>
2026-05-07 08:11:52 +0200

    Create DataWareHouse database and schemas
    This script creates a new database named 'DataWareHouse' and sets up three schemas: Bronze, Silver, and Gold. It includes a warning about data loss if the database already exists.

--- scripts/Init_database.sql
+++ scripts/Init_database.sql
@@ -1,1 +1,47 @@
+/* 
+==================================================
+Create Database & Schemas
+==================================================
+Purpose:
+	This script creates a new database named 'DataWareHouse' after checking if it already exists.
+	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+	within the DataWareHouse database namely: 
+	1. Bronze
+	2. Silver
+	3. Gold
+
+WARNING:
+	Running this scripts will drop the entire 'DataWareHouse' database if it exists.
+	All data in the database will be permanently deleted. Proceed with caution and
+	ensure you have proper backups before running this script.
+*/
+
+-- Creating Database "Data Warehouse"
+USE master;
+
+-- Drop & recreate the 'DataWareHouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE NAME = 'DataWareHouse')
+BEGIN
+	ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWareHouse;
+END;
+GO
+
+-- "Creating a new Database named "DataWareHouse"
+CREATE DATABASE DataWareHouse;
+
+USE DataWareHouse;
+GO
+
+-- ==========================================
+-- Creating Bronze, Silver & Gold Schema
+-- =========================================
+CREATE SCHEMA Bronze;
+GO
+
+CREATE SCHEMA Silver;
+GO
+
+CREATE SCHEMA Gold;
+GO
 
